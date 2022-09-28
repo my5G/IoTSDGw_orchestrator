078c5d215f75074e1e6767a311b5fbc86e39d330
rogerio-silva <rogeriosousaesilva@gmail.com>
2022-09-28 09:27:08 -0300

    rename orchestrator folder

--- orquestrators/Two_IotSDGw_Lorawan/smdb.sh
+++
@@ -1,3 +0,0 @@
-use free5gc
-db.policyData.ues.smData.remove({})
-db.policyData.ues.smData.insert({"smPolicySnssaiData" : { "01112233" : { "smPolicyDnnData" : { "internet" : { "dnn" : "internet" }, "ims" : { "dnn" : "ims" } }, "snssai" : { "sst" : 1, "sd" : "112233" } } }, "ueId" : "imsi-001010000000011" })
