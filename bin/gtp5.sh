5563d28862b6f63745ba46cd58980fff14ea261e
rogerio-silva <rogeriosousaesilva@gmail.com>
2022-09-28 14:41:37 -0300

    "Video Link and code fixes"

--- bin/gtp5.sh
+++ bin/gtp5.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-
+# Update S.O.
 sudo apt -y update
 sudo apt -y install git gcc g++ cmake autoconf libtool pkg-config libmnl-dev libyaml-dev
 
+# Clone repository
 git clone https://github.com/free5gc/gtp5g.git
+
+# Build GTP
 cd gtp5g
 make
 sudo make install
